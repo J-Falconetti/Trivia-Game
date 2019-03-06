2084ac8285df5df2100c38be7a40688c543fdd2f
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-05 23:34:08 -0500

    Questions and answers added to app.js file

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Star Wars Trivia game
 triva game that will ask true/flase question about Star Wars
 
 Below is varible names and what they are for:
+Var Gamquest - Set array for game questions
+    quest - Question to be answered
+    ans- sets possible asnwers
+    corans - sets correct answer
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,67 @@
-// this is the Javascript page
+// this ii the Questions
+var GameQuest = [
+    {
+        quest: "What is the name of Han Solo's ship?",
+        ans: {
+            a: 'Millennium Eagle',
+            b: 'Millennium Falcon',
+            c: 'The Hunk of Junk'
+        },
+        conans: 'b'
+    },
+    {
+        quest: "What class of fighter did Luke Skywalker Fly",
+        ans: {
+            a: 'Y-Wing',
+            b: 'A-Wing',
+            c: 'X-Wing'
+        },
+        conans: 'c'
+    },
+    {
+        quest: "What is a Parsec",
+        ans: {
+            a: 'A unt of time',
+            b: 'A unit of distance',
+            c: 'A made up phrase'
+        },
+        conans: 'b'
+    },
+    {
+        quest: "What is the name of the Star Wars Holliday Special?",
+        ans: {
+            a: 'There never was a specail',
+            b: 'Star Wars Holliday Special',
+            c: 'Yoda is Comming to Town'
+        },
+        conans: 'b'
+    },
+    {
+        quest: "What is Ben (Obi-Wan) Kenobi's rank durring the Clone Wars",
+        ans: {
+            a: 'General',
+            b: 'Privet First Class',
+            c: 'Jedi Knight'
+        },
+        conans: 'a'
+    },
+    {
+        quest: "What was Luke's home planet",
+        ans: {
+            a: 'Hoth',
+            b: 'Dagobah',
+            c: 'Tatooine'
+        },
+        conans: 'c'
+    },
+    {
+        quest: "Who/What is Yoda",
+        ans: {
+            a: 'Great warior / Jedi Master',
+            b: 'Sith',
+            c: 'Something you add Strawberries to for breakfast'
+        },
+        conans: 'a'
+    },
+];
+// stat functions
