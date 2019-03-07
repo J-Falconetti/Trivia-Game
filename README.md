076e003075f6e536ed5518822aa142f04ccb40f9
J-Falconetti <joseph.falconetti@gmail.com>
2019-03-07 09:57:10 -0500

    upadated ReadMe

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Star Wars Trivia game
 triva game that will ask 10 questions about Star Wars
 
+1) Timer for questions
+2) Questions on screen
+3) Answer choices
+4) check answer
+5) End game at either end of questions or end of time
+Display # right / # wrong
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,92 +1,47 @@
+var questright=0;
+var questworng=0;
+var GameQuest;
+
+
+
+function quest(ask, multi, final){
+    quest.ask=ask;
+    quest.multi=multi;
+    quest.final=final;
+
+}
+
+for (i=0; i < 11 ; i++)
+
+document.getElementById(<p id='quest'></p>)
+ 
+
+function rightchoice () {
+    if (multi === final)
+        qestsright++     
+}
+if else{
+    questwrong++
+
+}
+
+
+
+
 // this ii the Questions
 var GameQuest = [
-    {
-        quest: "What is the name of Han Solo's ship?",
-        ans: ['Millennium Eagle', 'Millennium Falcon','The Hunk of Junk']
-        corans: this.ans[1]
-    },
-    // change all questions to look like above
-    {
-        quest: "What class of fighter did Luke Skywalker Fly",
-        ans: {
-            a: 'Y-Wing',
-            b: 'A-Wing',
-            c: 'X-Wing'
-        },
-        corans: 'c'
-    },
-    {
-        quest: "What is a Parsec",
-        ans: {
-            a: 'A unt of time',
-            b: 'A unit of distance',
-            c: 'A made up phrase'
-        },
-        corans: 'b'
-    },
-    {
-        quest: "What is the name of the Star Wars Holliday Special?",
-        ans: {
-            a: 'There never was a specail',
-            b: 'Star Wars Holliday Special',
-            c: 'Yoda is Comming to Town'
-        },
-        corans: 'b'
-    },
-    {
-        quest: "What is Ben (Obi-Wan) Kenobi's rank durring the Clone Wars",
-        ans: {
-            a: 'General',
-            b: 'Privet First Class',
-            c: 'Jedi Knight'
-        },
-        corans: 'a'
-    },
-    {
-        quest: "What was Luke's home planet",
-        ans: {
-            a: 'Hoth',
-            b: 'Dagobah',
-            c: 'Tatooine'
-        },
-        corans: 'c'
-    },
-    {
-        quest: "Who/What is Yoda",
-        ans: {
-            a: 'Great warior / Jedi Master',
-            b: 'Sith',
-            c: 'Something you add Strawberries to for breakfast'
-        },
-        corans: 'a'
-    },
-    {
-        quest: "What were Han's odd for navigating the astroid field",
-        ans: {
-            a: '7284 : 1',
-            b: '10,000 : 1',
-            c: '3720 : 1'
-        },
-        corans: 'c'
-    },
-    {
-        quest: "What did Luke's uncle do on Tatooine",
-        ans: {
-            a: 'Moisture Farmer',
-            b: 'Droid repair',
-            c: 'Blue Mile farmer'
-        },
-        corans: 'a'
-    },
-    {
-        quest: "When is Star Wars Day",
-        ans: {
-            a: '18th of June',
-            b: '25th of December',
-            c: '4th of May'
-        },
-        corans: 'C'
-    },
-    
+        quest("What is the name of Han Solo's ship?", ['A: Millennium Eagle', 'B: Millennium Falcon','C: The Hunk of Junk'], "b"),
+        quest("What class of fighter did Luke Skywalker Fly?",["A: Y-Wing", "B: A-Wing", "C: X-Wing"], "c"),
+        quest("What is a Parsec?",["A: A unt of time", "B: A unit of distance", "C: A made up phrase"], "b"),
+        quest("What is the name of the Star Wars Holliday Special?",["A: There never was a specail","B: Star Wars Holliday Special", "C: Yoda is Comming to Town"], "b"),
+        quest("What is Ben (Obi-Wan) Kenobi's rank durring the Clone Wars",["A: General", "B: Privet First Class", "C: Jedi Knight"], "a"),
+        quest( "What was Luke's home planet", ["A: Hoth", "B:Dagobah", "C: Tatooine"], "c"),
+        quest("Who/What is Yoda", ["A: Great warior / Jedi Master", "B: Sith", "C: Something you add Strawberries to for breakfast"], "a"),
+        quest("What were Han's odds for navigating the astroid field", ["A: 7284 : 1","B: 10,000 : 1","C: 3720 : 1"], "c'"),
+        quest("What did Luke's uncle do on Tatooine",["A: Moisture Farmer", "B: Droid repair", "C: Blue Milk farmer"], "a"),
+        quest("When is Star Wars Day",["A: 18th of June", "B: 25th of December", "C: 4th of May"], "c"),
+
+
+        
 ];
 // stat functions
